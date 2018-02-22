f0d1f0128235a731fb4d92adc9fd1317041429b1
Dylan Stratton <dm.stratton94@gmail.com>
2018-02-22 14:15:32 -0600

    add button working

---
+++ shopping-list/index.js
@@ -0,0 +1,35 @@
+$(function() {
+
+    function createItem(itemText) {
+        return `
+        <li>
+            <span class="shopping-item">${itemText}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>
+        `;
+    }
+
+
+
+    /////////////
+    //ADD BUTTON
+    ////////////
+
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const userInput = $(event.target).find('.js-shopping-list-entry');
+        const userInputText = userInput.val();
+        userInput.val('');
+        console.log(userInputText);
+        const itemElement = createItem(userInputText);
+        $('.shopping-list').append(itemElement);
+    });
+
+});
